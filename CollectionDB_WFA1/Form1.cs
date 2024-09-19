eb283519a0412f622cb94bee1e649c9254f6a3e6
mlowr <mlowr@DESKTOP-R33IHFJ>
2024-09-19 11:51:39 -0500

    Add project files.

---
+++ CollectionDB_WFA1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollectionDB_WFA1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var m = new Form2();
+            m.Show();
+            string constr = ("Server=localhost;Database=VideoGameCollection;Trusted_Connection=True");
+            SqlConnection con = new SqlConnection(constr);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("dbo.Games_Add", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader rdr = cmd.ExecuteReader();
+        }
+    }
+    class SQLCon
+    {
+
+    }
+}
---
+++ CollectionDB_WFA1/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CollectionDB_WFA1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string constr = ("Server=localhost;Database=VideoGameCollection;Trusted_Connection=True");
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("dbo.Games_Add", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataReader rdr = cmd.ExecuteReader();
+            }
+        }
+    }
+}
